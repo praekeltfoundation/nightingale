0ab97d150b8d2661770ea07da94c4e7100c1c0d5
Kaitlyn Crawford <kaitlyn@praekelt.com>
2015-08-20 17:34:16 +0200

    added a view to display the existing submissions

--- ona/views.py
+++ ona/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+import json
+from .models import Submission
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
+from .serializers import SubmissionSerializer
 
-# Create your views here.
+class SubmissionViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows submissions to be viewed.
+    """
+    permission_classes = (IsAdminUser,)
+    queryset = Submission.objects.all()
+    serializer_class = SubmissionSerializer
